df4c97168df52d439a02f1f30410c1c69a813217
Jason Yang <jasony1@andrew.cmu.edu>
2019-07-26 12:55:53 -0400

    updates

--- CharlesPort/XavierExtract.py
+++ CharlesPort/XavierExtract.py
@@ -80,7 +80,7 @@ print("Done");
 # 	XavierFlowExtract.writeFlowMatlab(filename, flows, betas, counts, g2a, averages, rho, no, wavelength, mua, musp);
 # 	print("Completed Flow");
 
-def fullExtractMatlab(filename, averages, legacy=None, fs=2.5E6, intg=0.05, fsout=200, levels=16, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=None):
+def fullExtractMatlab(filename, averages, legacy=None, fs=None, intg=0.05, fsout=200, levels=16, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=None):
 	
 	dcsFilename = filename+"_DCS";
 	nirsFilename = filename+"_NIRS";
