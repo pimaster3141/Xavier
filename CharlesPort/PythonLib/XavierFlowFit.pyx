9c06df13132b6c31a7c7c456ff78f12cc8a6e164
Jason Yang <jasony1@andrew.cmu.edu>
2020-10-08 14:32:55 -0400

    fixed scaling

--- CharlesPort/PythonLib/XavierFlowFit.pyx
+++ CharlesPort/PythonLib/XavierFlowFit.pyx
@@ -7,12 +7,12 @@ import multiprocessing as mp
 from functools import partial
 import tqdm
 
-adB_BOUNDS = [1E-11 ,1E-6];
+aDb_BOUNDS = [1E-11 ,1E-6];
 BETA_BOUNDS = [0.01, 0.7];
 
-def G1Analytical(alpha, tauList, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10):
+def G1Analytical(aDb, tauList, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10):
 	k0=2*np.pi*no/(wavelength);
-	k=np.sqrt(3*mua*musp+6*musp*musp*k0*k0*alpha*tauList);
+	k=np.sqrt(3*mua*musp+6*musp*musp*k0*k0*aDb*tauList);
 	n=no/1;
 	Reff=-1.44/(n*n)+0.710/n+0.668+0.00636*n;
 	zb=(2*(1+Reff))/(3*musp*(1-Reff));
@@ -23,23 +23,28 @@ def G1Analytical(alpha, tauList, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, mu
 	g1=G1/G1_0;
 	return g1;
 
-def G2Analytical(alpha, beta, tauList, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10):
-	g1 = G1Analytical(alpha, tauList, rho, no, wavelength, mua, musp);
+def G2Analytical(aDb, beta, tauList, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10):
+	aDb = aDb * 1E-9;
+	g1 = G1Analytical(aDb, tauList, rho, no, wavelength, mua, musp);
 	return np.square(g1) * beta + 1;
 
-def G1Fit(g1Data, tauList, SNR, p0=1E-8, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10):
-	def f(tau, adB):
-		return G1Analytical(adB, tau, rho, no, wavelength, mua, musp)*SNR;
+# def G2Analytical_2Layer(aDb1, aDb2, beta, tauList, d=0.2, rho=2, no1=1.33, no2=1.33, wavelength=8.48E-5, mua1=0.1, mua2=0.1, musp1=10, musp2=10):
+# 	g1 = G1Analytical_2Layer(aDb1, aDb2, beta, tauList, d, rho, no1, no2, wavelength, mua1, mua2, musp1, musp2);
+# 	return np.squre(g1) * beta + 1;
 
-	(params, params_covariance) = optimize.curve_fit(f, tauList, g1Data*SNR, p0, bounds=adB_BOUNDS);
+def G1Fit(g1Data, tauList, SNR, p0=1E-8, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10):
+	def f(tau, aDb):
+		return G1Analytical(aDb, tau, rho, no, wavelength, mua, musp)*SNR;
+
+	(params, params_covariance) = optimize.curve_fit(f, tauList, g1Data*SNR, p0, bounds=aDb_BOUNDS);
 	return params;
 
-def G2Fit(g2Data, tauList, SNR, p0=[1E-9, 0.15], rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, ECC=False):
-	def f(tau, adB, beta):
-		return G2Analytical(adB, beta, tau, rho, no, wavelength, mua, musp)*SNR;
+def G2Fit(g2Data, tauList, SNR, p0=[1E-9/1E-9, 0.25], rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, ECC=False):
+	def f(tau, aDb, beta):
+		return G2Analytical(aDb, beta, tau, rho, no, wavelength, mua, musp)*SNR;
 
 	try:
-		(params, params_covariance) = optimize.curve_fit(f, tauList, g2Data*SNR, p0, bounds=((adB_BOUNDS[0], BETA_BOUNDS[0]), (adB_BOUNDS[1], BETA_BOUNDS[1])));
+		(params, params_covariance) = optimize.curve_fit(f, tauList, g2Data*SNR, p0, bounds=((aDb_BOUNDS[0]/1E-9, BETA_BOUNDS[0]), (aDb_BOUNDS[1]/1E-9, BETA_BOUNDS[1])));
 		return params;
 	except:
 		# print("fit Error:");
@@ -97,7 +102,7 @@ def flowFitDual(g2Data, tauList, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, mu
 	del(pool);
 	
 
-	return data[:, 0], data[:, 1];
+	return data[:, 0]*1E-9, data[:, 1];
 
 
 
